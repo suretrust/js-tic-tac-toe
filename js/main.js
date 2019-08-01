55f7ed217917e4d950a47076603b981c37ad9b5a
bellom <bellomsean@gmail.com>
2019-08-01 13:24:38 +0100

    added playGame function

--- js/main.js
+++ js/main.js
@@ -1,23 +1,29 @@
 
-const gameBoard = (() => {
-    let board = [":",":",":",":",":",":",":",":",":"];
-    const tableBody = document.getElementById('table-body');
+// checkforwin
+// checkfor draw
 
+const gameBoard = (() => {
+    let board = ["","","","","","","","",""];
+    const tableBody = document.getElementById('table-body');
+    const alert = document.getElementById('alert');
+    const showMessage = (mesg) => {
+        alert.innerHTML = mesg;
+    }
     const displayBoard = () => {
         tableBody.innerHTML += `<tr class="table-primary">
-                                    <td id="0" onClick="player${playGame.takeTurns()}.play(this.id)"><div class="py-3 display-1">${board[0]}</div></td>
-                                    <td id="1" onClick="player${playGame.takeTurns()}.play(this.id)"><div class="py-3 display-1">${board[1]}</div></td>
-                                    <td id="2" onClick="player${playGame.takeTurns()}.play(this.id)"><div class="py-3 display-1">${board[2]}</div></td>
+                                    <td id="0" onClick="playGame(this.id)"><div class="py-3 display-1">${board[0]}</div></td>
+                                    <td id="1" onClick="playGame(this.id)"><div class="py-3 display-1">${board[1]}</div></td>
+                                    <td id="2" onClick="playGame(this.id)"><div class="py-3 display-1">${board[2]}</div></td>
                                 </tr>
                                 <tr class="table-secondary">
-                                    <td id="3"><div class="py-3 display-1">${board[3]}</div></td>
-                                    <td id="4"><div class="py-3 display-1">${board[4]}</div></td>
-                                    <td id="5"><div class="py-3 display-1">${board[5]}</div></td>
+                                    <td id="3" onClick="playGame(this.id)"><div class="py-3 display-1">${board[3]}</div></td>
+                                    <td id="4" onClick="playGame(this.id)"><div class="py-3 display-1">${board[4]}</div></td>
+                                    <td id="5" onClick="playGame(this.id)"><div class="py-3 display-1">${board[5]}</div></td>
                                 </tr>
                                 <tr class="table-info">
-                                    <td id="6"><div class="py-3 display-1">${board[6]}</div></td>
-                                    <td id="7"><div class="py-3 display-1">${board[7]}</div></td>
-                                    <td id="8"><div class="py-3 display-1">${board[8]}</div></td>
+                                    <td id="6" onClick="playGame(this.id)"><div class="py-3 display-1">${board[6]}</div></td>
+                                    <td id="7" onClick="playGame(this.id)"><div class="py-3 display-1">${board[7]}</div></td>
+                                    <td id="8" onClick="playGame(this.id)"><div class="py-3 display-1">${board[8]}</div></td>
                                 </tr>`;
 
     }
@@ -26,27 +32,30 @@ const gameBoard = (() => {
        tableBody.innerHTML = "";
     }
     
-    return {board, displayBoard, resetBoard};
+    return {board, displayBoard, resetBoard, showMessage};
 })();
 
-const player = (name, symbol) => {
-    const play = (id) => {
-        gameBoard.board[id] = symbol;
+let player1 = "X";
+let player2 = "O";
+let currentPlayer = 1;
+let takeTurn = 0;
+
+function playGame(id){
+    takeTurn++;
+    if(currentPlayer == 1 && gameBoard.board[id] == ""){
+        gameBoard.board[id] = player1;
+        gameBoard.showMessage("Turn: Player 2");
         gameBoard.resetBoard();
         gameBoard.displayBoard();
+        currentPlayer++;
+    } else if (currentPlayer != 1 && gameBoard.board[id] == "") {
+        gameBoard.board[id] = player2;
+        gameBoard.showMessage("Turn: Player 1");
+        gameBoard.resetBoard();
+        gameBoard.displayBoard();
+        currentPlayer--;
     }
+}
 
-    return {name, symbol, play};
-};
-
-const playGame = (() => {
-    const takeTurns = () => {
-        
-    }
-    
-})();
-
-const player0 = player("Player One", 'X');
-const player1 = player("Player Two", 'O');
-
+gameBoard.showMessage("Turn: Player 1");
 gameBoard.displayBoard();
